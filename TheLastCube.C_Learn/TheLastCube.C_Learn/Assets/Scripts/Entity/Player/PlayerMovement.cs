6820a3d836bd34ef3cd6d74f6bf9e1d85412451b
leehun1997 <81948783+leehun1997@users.noreply.github.com>
2024-11-01 20:53:16 +0900

    [Feature] Cube Roll

--- TheLastCube.C_Learn/TheLastCube.C_Learn/Assets/Scripts/Entity/Player/PlayerMovement.cs
+++ TheLastCube.C_Learn/TheLastCube.C_Learn/Assets/Scripts/Entity/Player/PlayerMovement.cs
@@ -1,15 +1,23 @@
 using Common.Yield;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Vector3 fixPosition;
+
     private PlayerController cubeController;
     public LayerMask groundlayerMask;
+    private bool isMoving = false;
 
     [SerializeField]private float rotateSpeed;
-    public float rotateRate;
+    private float rotateRate;
+    private int rollBackInt=10;
+
+    [SerializeField] private int skillCount;
 
     private void Awake()
     {
@@ -17,18 +25,56 @@ public class PlayerMovement : MonoBehaviour
     }
     private void Start()
     {
+        rotateRate = 90 / rotateSpeed;
+
         cubeController.OnMoveEvent += Move;
     }
 
 
     private void Move(Vector2 direction)
     {
-        Debug.Log(direction);
+        if (isMoving)
+        {
+            Debug.Log("Err Moving!");
+            return;
+        }
+        //Debug.Log(direction);
+        if (CheckWall(direction))
+        {
+            return;
+        }
+
         Vector3 ancher = transform.position + (new Vector3(direction.x, -1, direction.y)) * 0.5f;//회전시킬 위치
         Vector3 axis = Vector3.Cross(Vector3.up, new Vector3(direction.x, 0, direction.y));//두선과 수직인 회전시킬 축을 찾기
-        Debug.Log($"axis :  {axis}");
+        //Debug.Log($"axis :  {axis}");
+
+        if (!CheckNextGround(direction))
+        {
+            if (cubeController.skillActive)
+            {
+                StartCoroutine(RollDown(ancher, axis));
+                return;
+            }
+            StartCoroutine(RollBack(ancher, axis));
+            return;
+        }
 
         StartCoroutine(Roll(ancher, axis));
+        
+    }
+
+    private bool CheckWall(Vector2 direction)
+    {
+        Vector3 dir = new Vector3(direction.x, 0, direction.y);
+        Ray ray = new Ray(transform.position + dir * 0.5f, dir);
+        //Debug.DrawRay(transform.position - (new Vector3(0,0.5f,0)), Vector3.down, Color.red);
+
+        if (Physics.Raycast(ray, 1f, groundlayerMask))
+        {
+            return true;
+        }
+
+        return false;
     }
 
     private bool CheckNextGround(Vector2 direction)
@@ -36,17 +82,17 @@ public class PlayerMovement : MonoBehaviour
         Ray ray = new Ray(transform.position + (new Vector3(direction.x, -0.5f, direction.y)), Vector3.down);
         //Debug.DrawRay(transform.position - (new Vector3(0,0.5f,0)), Vector3.down, Color.red);
 
-        if (Physics.Raycast(ray, 0.1f, groundlayerMask))
+        if (Physics.Raycast(ray, 1f, groundlayerMask))
         {
             return true;
         }
+
         return false;
     }
 
     IEnumerator Roll(Vector3 ancher, Vector3 axis)
     {
-        cubeController.isMoving = true;
-        rotateRate = 90 / rotateSpeed;
+        isMoving = true;
 
         for (int i = 0; i < rotateRate; i++)
         {
@@ -54,11 +100,41 @@ public class PlayerMovement : MonoBehaviour
             yield return YieldCache.WaitForSeconds(0.01f);
         }
 
-        cubeController.isMoving = false;
+        isMoving = false;
     }
 
-    void CubeDown()
+    IEnumerator RollBack(Vector3 ancher, Vector3 axis)
     {
-        transform.position += Vector3.down;
+        isMoving = true;
+
+        for (int i = 0; i < rollBackInt; i++)//회전이후 축이 틀어지지 않도록 고정된 int값을 사용.
+        {
+            transform.RotateAround(ancher, axis, rotateSpeed);
+            yield return YieldCache.WaitForSeconds(0.01f);
+        }
+        for (int i = 0; i < rollBackInt; i++)
+        {
+            transform.RotateAround(ancher, axis, -rotateSpeed);
+            yield return YieldCache.WaitForSeconds(0.01f);
+        }
+
+        isMoving = false;
+    }
+
+    IEnumerator RollDown(Vector3 ancher, Vector3 axis)
+    {
+        isMoving = true;
+
+        Vector3 downVec = ((transform.position -  ancher) * 2) / rotateRate;
+        fixPosition = transform.position + ancher * 2;
+
+        for (int i = 0; i < rotateRate; i++)
+        {
+            transform.RotateAround(transform.position, axis, rotateSpeed);//지정한 점을 통과하는 벡터를 중심으로 회전 
+            transform.position -= downVec;
+            yield return new WaitForFixedUpdate();
+        }
+
+        isMoving = false;
     }
 }
---
+++ TheLastCube.C_Learn/TheLastCube.C_Learn/Assets/Scripts/Entity/Player/PlayerSkill.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSkill : MonoBehaviour
+{
+    private PlayerController cubeController;
+
+    private Transform pastPosition;
+
+    private void Awake()
+    {
+        cubeController = GetComponent<PlayerController>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cubeController.OnSkillEvent += skill1;
+    }
+
+    private void skill1(bool active)
+    {
+        if (active)
+        {
+            Debug.Log("Position Save");
+            pastPosition.position = gameObject.transform.position;
+            return;
+        }
+        else
+        {
+            Debug.Log("Load Past Position");
+            this.gameObject.transform.position = pastPosition.transform.position;
+            pastPosition.position = Vector3.zero;
+            return;
+        }
+    }
+}
