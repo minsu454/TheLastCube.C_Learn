bdda6fcf5df0d58525fb35116b62531d6c2a341d
minsu454 <kminsu454@gmail.com>
2024-11-05 12:32:09 +0900

    [Temp]

---
+++ TheLastCube.C_Learn/TheLastCube.C_Learn/Assets/TheLastCube/Scripts/UI/Parts/UIScreenViewButton1.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using TMPro;
+using UnityEngine;
+
+//public class UIScreenViewButton : MonoBehaviour
+//{
+//    private Enum enumType;
+//    [SerializeField] private TextMeshProUGUI text;
+//    public event Action<Enum> OnClickEvent;
+    
+//    public void Init(Enum enumType)
+//    {
+//        this.enumType = enumType;
+//        text.text = enumType.ToString();
+//    }
+
+//    public void OnButton()
+//    {
+//        OnClickEvent?.Invoke(enumType);
+//    }
+//}
