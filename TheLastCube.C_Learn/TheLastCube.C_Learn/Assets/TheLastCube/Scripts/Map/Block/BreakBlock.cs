f80424e3bdee653954cf6182bb545b1da894b0c2
whdgur2498 <whdgur2498@gmail.com>
2024-11-06 19:14:09 +0900

    [Update] BreakBlock

--- TheLastCube.C_Learn/TheLastCube.C_Learn/Assets/TheLastCube/Scripts/Map/Block/BreakBlock.cs
+++ TheLastCube.C_Learn/TheLastCube.C_Learn/Assets/TheLastCube/Scripts/Map/Block/BreakBlock.cs
@@ -11,25 +11,6 @@ public class BreakBlock : MapBlock
         GroundRenderer.material = Managers.Material.Return(data.MoveType);
     }
 
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    if (IsPlayer(other))
-    //    {
-    //        PlayerController playerController = other.GetComponent<PlayerController>();
-    //        if (playerController != null)
-    //        {
-    //            Debug.Log("스킬:" + playerController.skillActive);
-    //            Debug.Log("충돌이 감지 되었습니다");
-
-    //            if (playerController.skillActive && playerController.yellowSkill)
-    //            {
-    //                gameObject.SetActive(false);
-    //                Debug.Log("통과했습니다");
-    //            }
-    //        }
-    //    }
-    //}
-
     private bool IsPlayer(Collider other)
     {
         return playerLayer == (playerLayer | (1 << other.gameObject.layer));
