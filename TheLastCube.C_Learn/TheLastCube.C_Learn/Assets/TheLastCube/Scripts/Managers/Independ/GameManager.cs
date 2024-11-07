a7882ca4ef389d88eec20c8b07c5f40f217c3637
leehun1997 <81948783+leehun1997@users.noreply.github.com>
2024-11-07 16:13:40 +0900

    debug delete

--- TheLastCube.C_Learn/TheLastCube.C_Learn/Assets/TheLastCube/Scripts/Managers/Independ/GameManager.cs
+++ TheLastCube.C_Learn/TheLastCube.C_Learn/Assets/TheLastCube/Scripts/Managers/Independ/GameManager.cs
@@ -99,7 +99,7 @@ public class GameManager : MonoBehaviour
             return;
         }
         
-        Debug.Log(list.Count);
+        //Debug.Log(list.Count);
         for(int i =0;i<list.Count; i++)
         {
             list[i].OnEvent();
