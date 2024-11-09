5338f3e748e13a980033fb8089b940e76fd62dc6
leehun1997 <81948783+leehun1997@users.noreply.github.com>
2024-11-09 12:25:24 +0900

    [Add] Description

--- TheLastCube.C_Learn/TheLastCube.C_Learn/Assets/TheLastCube/Scripts/Entity/Player/PlayerController.cs
+++ TheLastCube.C_Learn/TheLastCube.C_Learn/Assets/TheLastCube/Scripts/Entity/Player/PlayerController.cs
@@ -8,12 +8,12 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : TopDownController
 {
-    [SerializeField] private int skillCount;
-    public PlayerMovement playerMovement;
-    public PlayerQuadController playerQuadController;
-    public PlayerSkill playerSkill;
-    public GameObject Effect;
-    private Vector2 direction = Vector2.zero;
+    [SerializeField] private int skillCount; //redskillcount
+    public PlayerMovement playerMovement; // *아래 3가지 모두 프리팹으로 만들어질 플레이어의 관계를 위해서 
+    public PlayerQuadController playerQuadController; // *PlayerController가 버스의 느낌을 해주고 있는 것 
+    public PlayerSkill playerSkill; // *다른 스크립트에서 또 다른 스크립트의 정보를 얻기 위해서는 PlayerController의 무언가로 접근하면 된다. 
+    public GameObject Effect; //파티클을 다루가 위해서 필요한 정보
+    private Vector2 direction = Vector2.zero; //기본값 int = 0 같은 느씸
 
     public bool skillActive = false;
     public bool redSkill = false;
@@ -28,7 +28,7 @@ public class PlayerController : TopDownController
         {
             return;            
         }
-        if (yellowSkill)
+        if (yellowSkill)//yellowSkill이 켜져있으면 일반 이동보다 우선적으로 실행하도록
         {
             CallSpecialMoveEvent(direction);
             return;
@@ -37,15 +37,15 @@ public class PlayerController : TopDownController
         CallMoveEvent(direction);
     }
 
-    public void OnMove(InputValue value)
+    public void OnMove(InputValue value)//input system을 이용하여 vector value를 받음
     {
         direction = value.Get<Vector2>();
-        if (direction == Vector2.zero)
+        if (direction == Vector2.zero)//입력을 안하고 있을때 불필요한 수행 방지
         {
             isPressing = false;
             return;
         }
-        if (direction.x * direction.y != 0f)
+        if (direction.x * direction.y != 0f)//대각선 이동 방지
         {
             isPressing = false;
             return;
@@ -55,17 +55,17 @@ public class PlayerController : TopDownController
 
     public void OnSkill(InputValue value)
     {
-        if (playerMovement.Moving()) return;
+        if (playerMovement.Moving()) return;//이동 중에 스킬 발동 불가
         if (value.isPressed)
         {
-            if (skillActive)
+            if (skillActive)// 스킬 다시 발동 시
             {
                 skillActive = false;
                 CallSkillEvent(skillActive);
             }
-            else
+            else//처음 능력을 발동 시
             {
-                if (!playerSkill.CheckSkiilType()) return;
+                if (!playerSkill.CheckSkiilType()) return; // 구현이 안된 능력이거나 없을 경우
                 skillActive = true;
 
                 CallSkillEvent(skillActive);
@@ -73,7 +73,7 @@ public class PlayerController : TopDownController
         }
     }
 
-    public void OnClick(InputValue value)
+    public void OnClick(InputValue value)//마우스 좌클릭시 값을 EventManager에 전달해줌 (역 구독?)
     {
         EventManager.Dispatch(GameEventType.ChangeViewPoint, null);
     }
--- TheLastCube.C_Learn/TheLastCube.C_Learn/Assets/TheLastCube/Scripts/Entity/Player/TopDownController.cs
+++ TheLastCube.C_Learn/TheLastCube.C_Learn/Assets/TheLastCube/Scripts/Entity/Player/TopDownController.cs
@@ -6,9 +6,9 @@ using UnityEngine.InputSystem;
 
 public class TopDownController : MonoBehaviour
 {
-    public event Action<Vector2> OnMoveEvent;
-    public event Action<Vector2> OnSpecialMoveEvent;
-    public event Action<bool> OnSkillEvent;
+    public event Action<Vector2> OnMoveEvent; //플레이어가 움직일 때 발생시킬 event들
+    public event Action<Vector2> OnSpecialMoveEvent; //노란색 스킬이 켜졌을 때 발생시킬 event들(사실상 빠른 이동이고 이후에 기능들이 추가 될 수 있기에 나눔)
+    public event Action<bool> OnSkillEvent; // 플레이어가 스킬(e) 를 눌렀을 때 발생시킬 event
 
     protected bool isPressing = false;
 
